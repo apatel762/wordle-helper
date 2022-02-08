800f51cd0b65ba52089c631d1fe83a8782bc13d6
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-02-08 18:16:45 +0000

    add logic for filtering words

--- wordle.py
+++ wordle.py
@@ -34,7 +34,7 @@ def clean(s: str, whitelisted_chars: str = string.ascii_letters) -> str:
 
 def validate_guess(s: str, check_dictionary: bool = True) -> bool:
     if len(s) != 5:
-        print(f"Bad guess: {s} (not five letters long")
+        print(f"Bad guess: {s} (not five letters long)")
         return False
 
     if check_dictionary and s not in words:
@@ -44,9 +44,11 @@ def validate_guess(s: str, check_dictionary: bool = True) -> bool:
     return True
 
 
-INCORRECT = " "
-YELLOW = "Y"
-GREEN = "G"
+INCORRECT: str = " "
+YELLOW: str = "Y"
+GREEN: str = "G"
+
+ALLOWED_RESULTS_CHARS: str = "".join([INCORRECT, YELLOW, GREEN]).lower()
 
 
 def map_result_to_emoji(result_char: str) -> str:
@@ -68,8 +70,27 @@ def beautify_results(r: str) -> str:
     return "".join([map_result_to_emoji(c) for c in r])
 
 
+def filter_word_list(
+    word_list: List[str], guessed_word: str, guess_result: str
+) -> None:
+    if not validate_guess(s=guess_result, check_dictionary=False):
+        raise RuntimeError("cannot filter dictionary with invalid guess result")
+
+    for index, pair in enumerate(zip(guessed_word, guess_result)):
+        letter: str = pair[0]
+        result: str = pair[1]
+        if result == INCORRECT:
+            word_list = [w for w in word_list if letter not in w]
+        elif result == YELLOW:
+            word_list = [w for w in word_list if letter in w]
+        elif result == GREEN:
+            word_list = [w for w in word_list if w[index] == letter]
+
+    return word_list
+
+
 if __name__ == "__main__":
-    number_of_guesses: int = 0
+    # number_of_guesses: int = 0
     is_valid: bool = False
     guess: str = ""
     while not is_valid:
@@ -81,7 +102,7 @@ if __name__ == "__main__":
     result: str = ""
     while not is_valid:
         answers = inquirer.prompt([inquirer.Text("result", message="result")])
-        result: str = clean(answers["result"], whitelisted_chars=" yg")
+        result: str = clean(answers["result"], whitelisted_chars=ALLOWED_RESULTS_CHARS)
         is_valid = validate_guess(result, check_dictionary=False)
 
     # pretty-print the current guess & result
@@ -93,3 +114,7 @@ if __name__ == "__main__":
     # show the top 10 most likely guesses based on the information we have
     #   need to find a list of the most common english 5-letter words from somewhere
     # rinse and repeat
+    print("filtering results...")
+    words: List[str] = filter_word_list(
+        word_list=words, guessed_word=guess, guess_result=result
+    )
